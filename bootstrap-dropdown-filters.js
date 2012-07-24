e32dfa331d5ee4baf5f1f6f4ead44eceef284869
Luiz Faias Junior <luizfaias@gmail.com>
2012-07-24 10:12:32 -0300

    adding screenshot

--- bootstrap-dropdown-filters.js
+++ bootstrap-dropdown-filters.js
@@ -3,11 +3,11 @@
 		return this.each(function(i, e){
 			if (!($(e).data('convert') == 'no')) {
 				$(e).hide().wrap('<div class="btn-group" id="select-group-' + i + '" />');
-				var elementId = $(e).attr('id');
 				var select = $('#select-group-' + i);
 				var current = ($(e).val()) ? $(e).val(): '&nbsp;';
 				var textValue = '';
 	
+				var elementId = $(e).attr('id');
 				var $label = $("[for='"+elementId+"']");
 				var label = $label.text();
 				$label.hide();
@@ -18,18 +18,18 @@
 					}
 				});
 				
-				select.html('<input type="hidden" value="' + $(e).val() + '" name="' + $(e).attr('name') + '" id="' + $(e).attr('id') + '" class="' + $(e).attr('class') + '" /><a class="btn" href="javascript:;"><span class="select-title"></span><span class="select-value">' + textValue + '</span></a><a class="btn dropdown-toggle" data-toggle="dropdown" href="javascript:;"><i class="caret"></i></a><ul class="dropdown-menu"></ul>');
+				select.html('<input type="hidden" onchange="' + $(e).attr('onchange') + '" value="' + $(e).val() + '" name="' + $(e).attr('name') + '" id="' + $(e).attr('id') + '" class="' + $(e).attr('class') + '" /><a class="btn" href="javascript:;"><span class="filter-title"></span><span class="filter-value">' + textValue + '</span></a><a class="btn dropdown-toggle" data-toggle="dropdown" href="javascript:;"><i class="caret"></i></a><ul class="dropdown-menu"></ul>');
 				$(e).find('option').each(function(o,q) {
 					var text = $(q).text();
 					select.find('.dropdown-menu').append('<li><a href="javascript:;" data-value="' + $(q).attr('value') + '">' + text + '</a></li>');
 					if ($(q).attr('selected')) select.find('.dropdown-menu li:eq(' + o + ')').click();
 				});
 				
-				select.find('.select-title').text(label + ' ');
+				select.find('.filter-title').text(label + ' ');
 				
 				select.find('.dropdown-menu a').click(function() {
 					select.find('input[type=hidden]').val($(this).data('value')).change();
-					select.find('.btn:eq(0) .select-value').text($(this).text());
+					select.find('.btn:eq(0) .filter-value').text($(this).text());
 				});
 			}
 		});
